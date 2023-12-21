4f7749ac40047d9c7d3d3783dcd107aa7916fbb4
bhoomika <bhoomikaganji2002@mail.com>
2023-12-21 07:30:06 +0530

    commit message

---
+++ javaexpression/src/javaexpression/Javaexpression3.java
@@ -0,0 +1,13 @@
+package javaexpression;
+
+public class Javaexpression3 {
+	public static void main(String[] args) {
+		int x=4,a=10,b=6,c=3;
+		System.out.println(Math.exp(2*x-4*x));
+		System.out.println(Math.sqrt(Math.sin(a)+Math.tan(a))-Math.exp(2*x));
+		System.out.println(5/10 + Math.pow(x,5) + 4/b*Math.pow(x,3));
+		System.out.println(a*Math.pow(x,5) + b*Math.pow(x,3) + c);
+		
+	}
+
+}
